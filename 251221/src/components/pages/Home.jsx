7573dd3a97bd532c1e2ba18cb1741a0213d5c1f2
nurgnc <gencbedriyenur@gmail.com>
2021-12-27 18:21:53 +0300

    25.12.21 lesson updated

--- 251221/src/components/pages/Home.jsx
+++ 251221/src/components/pages/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PostSearch from './PostSearch';
-import Faker from 'faker/locale/tr';
+import Faker from 'faker/locale/tr'; //rastgele bilgiler için kullanılabilir
 
 function Home(props) {
+
+  function runApi() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const posts = [...Array(10)].map((item,index) => {
+      const post = {title: Faker.name.findName(), 
+        author: Faker.name.firstName()}
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(post),
+        redirect: "follow"
+      };
+      
+      fetch("http://localhost:4000/posts", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.error("error", error));
+
+        return post
+    })
+  }
+
   return (
     <>
       <h1>Ana Sayfa</h1>
+      <button onClick={runApi}>Click Me</button>
       <ul>
         {[...Array(10)].map((item, index) => (
           <li key={index}>
