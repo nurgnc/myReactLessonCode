09d415cdf9dd102f4bc9eb5082cc5269434ab34f
Bedriye Nur GENÇ <gencbedriyenur@gmail.com>
2022-01-12 23:05:58 +0300

    Update BaseButton.jsx

--- Styled Components 08.01.22/src/styledComponents/BaseButton.jsx
+++ Styled Components 08.01.22/src/styledComponents/BaseButton.jsx
@@ -3,9 +3,9 @@ import { baseCss, padding } from './baseCss';
 
 const BaseButton = styled.button`
   color: ${({ theme }) => theme.color};
-  background-color: ${(props) => props.theme.backgroundColor}
-  ${padding}
-  ${baseCss}
+  background-color: ${(props) => props.theme.backgroundColor};
+  ${padding};
+  ${baseCss};
   cursor: pointer;
   text-decoration: none;
   border: 1px solid #222;
